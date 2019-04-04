aa19ba3560d11d6853076b9f56fb2b1a13f1244f
Namlh1912 <namlh1912@gmail.com>
2019-04-04 11:59:20 +1100

    Initial Commit

---
+++ Journal/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Journal
+//
+//  Created by Hai Le on 4/4/19.
+//  Copyright © 2019 Hai Le. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Journal/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Journal
+//
+//  Created by Hai Le on 4/4/19.
+//  Copyright © 2019 Hai Le. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
