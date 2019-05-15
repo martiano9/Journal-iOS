66689adf09978235fa11d7fd3d13932ec4449237
Hai Le <hai@carrybible.com>
2019-05-15 16:37:54 +1000

    demo chart

---
+++ Journal/Models/DateValueFormatter.swift
@@ -0,0 +1,23 @@
+//
+//  DateValueFormatter.swift
+//  Journal
+//
+//  Created by Hai Le on 15/5/19.
+//  Copyright © 2019 Hai Le. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+public class DateValueFormatter: NSObject, IAxisValueFormatter {
+    private let dateFormatter = DateFormatter()
+    
+    override init() {
+        super.init()
+        dateFormatter.dateFormat = "dd MMM"
+    }
+    
+    public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        return dateFormatter.string(from: Date(timeIntervalSince1970: value))
+    }
+}
--- Journal/ViewControllers/HistoryViewController.swift
+++ Journal/ViewControllers/HistoryViewController.swift
@@ -11,27 +11,97 @@ import Charts
 
 class HistoryViewController: UIViewController {
 
-    @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet var chartView: LineChartView!
+    @IBOutlet var sliderX: UISlider!
+    @IBOutlet var sliderTextX: UITextField!
     var lastPeriodLength: Int = 30
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        self.title = "History"
+        
+        chartView.delegate = self
+        
+        chartView.chartDescription?.enabled = false
+        
+        chartView.dragEnabled = false
+        chartView.setScaleEnabled(false)
+        chartView.pinchZoomEnabled = false
+        chartView.highlightPerDragEnabled = false
+        
+        chartView.backgroundColor = #colorLiteral(red: 0.9560336471, green: 0.9608393312, blue: 0.9693661332, alpha: 1)
+        
+        chartView.legend.enabled = true
+        
+        let xAxis = chartView.xAxis
+        xAxis.labelPosition = .bottom
+        xAxis.labelFont = .systemFont(ofSize: 10, weight: .light)
+        xAxis.labelTextColor = .red
+        xAxis.drawAxisLineEnabled = false
+        xAxis.drawGridLinesEnabled = false
+        xAxis.centerAxisLabelsEnabled = true
+        xAxis.granularity = 3600
+        xAxis.valueFormatter = DateValueFormatter()
+        
+        let leftAxis = chartView.leftAxis
+        leftAxis.labelPosition = .outsideChart
+        leftAxis.labelFont = .systemFont(ofSize: 12, weight: .light)
+        leftAxis.drawGridLinesEnabled = true
+        leftAxis.granularityEnabled = true
+        leftAxis.axisMinimum = 0
+        leftAxis.axisMaximum = 6
+        leftAxis.yOffset = 0
+        leftAxis.labelTextColor = .blue
+        
+        
+        chartView.rightAxis.enabled = false
+        
+        chartView.legend.form = .line
+        
+        sliderX.value = 100
+        slidersValueChanged(nil)
+        
+        chartView.animate(xAxisDuration: 2.5)
     }
     
-    func renderChart() {
+    func setDataCount(_ count: Int, range: UInt32) {
+        let now = Date().timeIntervalSince1970
+        let hourSeconds: TimeInterval = 3600
         
+        let from = now - (Double(count) / 2) * hourSeconds
+        let to = now + (Double(count) / 2) * hourSeconds
+        
+        let values = stride(from: from, to: to, by: hourSeconds).map { (x) -> ChartDataEntry in
+            let y = arc4random_uniform(range) + 1
+            return ChartDataEntry(x: x, y: Double(y))
+        }
+        
+        let set1 = LineChartDataSet(entries: values, label: "Mood")
+        set1.axisDependency = .left
+        set1.setColor(#colorLiteral(red: 0.001910516527, green: 0.3388788402, blue: 0.8543916345, alpha: 1))
+        set1.lineWidth = 2
+        set1.drawCirclesEnabled = false
+        set1.drawValuesEnabled = false
+        set1.fillAlpha = 0.26
+        set1.fillColor = .blue
+        set1.highlightColor = .red
+        set1.drawCircleHoleEnabled = false
+        
+        let data = LineChartData(dataSet: set1)
+        data.setValueTextColor(.red)
+        data.setValueFont(.systemFont(ofSize: 9, weight: .light))
+        
+        chartView.data = data
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func slidersValueChanged(_ sender: Any?) {
+        sliderTextX.text = "\(Int(sliderX.value))"
+        self.setDataCount(Int(sliderX.value), range: 5)
     }
-    */
+}
 
+extension HistoryViewController: ChartViewDelegate {
+    
 }
